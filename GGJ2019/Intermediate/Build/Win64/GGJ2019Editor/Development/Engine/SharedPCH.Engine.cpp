d1daa893da84ec771f1415d00f36e48c85967e52
8utt8ag <tassamorth@gmail.com>
2019-01-26 05:35:48 +0000

    Added Skel meshes and walk anims

--- GGJ2019/Intermediate/Build/Win64/GGJ2019Editor/Development/Engine/SharedPCH.Engine.cpp
+++ GGJ2019/Intermediate/Build/Win64/GGJ2019Editor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-#include "C:/Users/p018236h.STUDENT/Documents/GameJam/GGJ2019/GGJ2019/Intermediate/Build/Win64/GGJ2019Editor/Development/Engine/SharedPCH.Engine.h"
-=======
-#include "C:/Users/w017177h.STUDENT/Documents/Github/GGJ2019/GGJ2019/Intermediate/Build/Win64/GGJ2019Editor/Development/Engine/SharedPCH.Engine.h"
->>>>>>> JohnBranch
+#include "C:/Users/t020251g/Documents/GGJ2019/GGJ2019/Intermediate/Build/Win64/GGJ2019Editor/Development/Engine/SharedPCH.Engine.h"
--- GGJ2019/Intermediate/Build/Win64/UE4Editor/Inc/GGJ2019/GGJ2019.init.gen.cpp
+++ GGJ2019/Intermediate/Build/Win64/UE4Editor/Inc/GGJ2019/GGJ2019.init.gen.cpp
@@ -19,17 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeGGJ2019_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/GGJ2019",
 				PKG_CompiledIn | 0x00000000,
-<<<<<<< HEAD
-<<<<<<< HEAD
-				0xAC4D4974,
-=======
-				0xEADE4C08,
->>>>>>> AngBranch
-				0x0551F4BE,
-=======
-				0xA462C08C,
-				0xF4C022BD,
->>>>>>> JohnBranch
+				0x4AB33268,
+				0x7B235303,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
