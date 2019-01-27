3983968166c919145e6d02952d621163830b6bb1
Ang <angharadjanehill@gmail.com>
2019-01-27 15:36:57 +0000

    Lighting Fix

--- GGJ2019/Intermediate/Build/Win64/GGJ2019Editor/Development/Engine/SharedPCH.Engine.cpp
+++ GGJ2019/Intermediate/Build/Win64/GGJ2019Editor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-#include "C:/Users/w017177h.STUDENT/Documents/Github/GGJ2019/GGJ2019/Intermediate/Build/Win64/GGJ2019Editor/Development/Engine/SharedPCH.Engine.h"
-=======
-#include "C:/Users/w018030h.STUDENT/Documents/newGGJ2019/GGJ2019/GGJ2019/Intermediate/Build/Win64/GGJ2019Editor/Development/Engine/SharedPCH.Engine.h"
->>>>>>> MattbranchNew
-=======
-#include "C:/Users/p018236h.STUDENT/Documents/GameJam/GGJ2019/GGJ2019/Intermediate/Build/Win64/GGJ2019Editor/Development/Engine/SharedPCH.Engine.h"
->>>>>>> NewNewNewRhiannonBranch
-=======
 #include "C:/Users/h017026h/Documents/Game Jame/GGJ2019/GGJ2019/Intermediate/Build/Win64/GGJ2019Editor/Development/Engine/SharedPCH.Engine.h"
->>>>>>> Cine_Camera_Branch
